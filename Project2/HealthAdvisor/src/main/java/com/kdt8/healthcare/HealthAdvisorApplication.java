0fc4000b7ba29f317135a8f98e0028ae5122716e
jwahyunkim <largoing@gmail.com>
2025-03-06 16:11:17 +0900

    Remove embedded repository

---
+++ Project2/HealthAdvisor/src/main/java/com/kdt8/healthcare/HealthAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.kdt8.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAdvisorApplication.class, args);
+	}
+
+}
---
+++ Project2/HealthAdvisor/src/test/java/com/kdt8/healthcare/HealthAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdt8.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
